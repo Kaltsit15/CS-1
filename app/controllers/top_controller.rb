694808dd56c0a2c044c56157717681fec436304b
Mochida <2111140020j@ed.fuk.kindai.ac.jp>
2023-10-24 05:16:48 +0000

    b

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -8,17 +8,12 @@ class TopController < ApplicationController
   end
 
   def login
-    if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
+    if  params[:uid] == "kindai" and params[:pass] == "sanriko"
       session[:login_uid] = params[:uid]
       redirect_to top_main_path
 
     else
       render "error"
     end
-  end
-  
-  def logout
-    session.delete(:login_uid)
-    redirect_to top_main_path
   end
 end
